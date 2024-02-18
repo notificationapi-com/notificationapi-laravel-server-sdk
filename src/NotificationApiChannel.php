2c733350f704b46bb9379b417ac591ef5703adad
Nairan Omura <nairanomura@hotmail.com>
2024-02-17 21:48:30 -0300

    Add files via upload

---
+++ src/NotificationApiChannel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NotificationAPI;
+
+
+use Illuminate\Notifications\Notification;
+
+class NotificationApiChannel
+{
+    public function send(mixed $notifiable, Notification $notification): bool
+    {
+        return (new NotificationApiService)->send($notification->toNotificationApi($notifiable));
+    }
+}
