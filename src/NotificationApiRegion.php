f148851deaf6bd8dbbd78070b7d96f3b08f14884
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2025-03-03 22:07:17 -0330

    Add NotificationApiRegion class for managing API base URLs by region

---
+++ src/NotificationApiRegion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NotificationAPI;
+
+class NotificationApiRegion
+{
+    public const US_REGION = 'US';
+    public const EU_REGION = 'EU';
+    public const CA_REGION = 'CA';
+
+    private const BASE_URLS = [
+        self::US_REGION => 'https://api.notificationapi.com',
+        self::EU_REGION => 'https://api.eu.notificationapi.com',
+        self::CA_REGION => 'https://api.ca.notificationapi.com',
+    ];
+
+    public static function getBaseUrl(string $region): string
+    {
+        if (!isset(self::BASE_URLS[$region])) {
+            throw new \InvalidArgumentException("Invalid region: {$region}. Must be one of: " . implode(', ', array_keys(self::BASE_URLS)));
+        }
+        return self::BASE_URLS[$region];
+    }
+} 
