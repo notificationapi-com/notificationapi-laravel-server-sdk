7821e79e6ced0358bdf246b4eb5c8cde6520cdd9
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2025-03-03 22:06:46 -0330

    Enhance NotificationApiService constructor with flexible configuration

--- src/NotificationApiService.php
+++ src/NotificationApiService.php
@@ -4,11 +4,30 @@ namespace NotificationAPI;
 
 class NotificationApiService extends NotificationAPI
 {
-    public function __construct()
+    public function __construct(array $config = [])
     {
+        $config = array_merge(
+            config('services.notification-api', []),
+            $config
+        );
+
+        if (empty($config['key'])) {
+            throw new \InvalidArgumentException('NotificationAPI key is required');
+        }
+
+        if (empty($config['secret'])) {
+            throw new \InvalidArgumentException('NotificationAPI secret is required');
+        }
+
+        $baseUrl = null;
+        if (!empty($config['region'])) {
+            $baseUrl = NotificationApiRegion::getBaseUrl($config['region']);
+        }
+
         parent::__construct(
-            config('services.notification-api.key'),
-            config('services.notification-api.secret')
+            $config['key'],
+            $config['secret'],
+            $baseUrl
         );
     }
 }
